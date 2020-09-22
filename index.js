8329b4fafe9370d29bd352ac04d3f5a1d5212d87
so0choi <simc2644@gmail.com>
2020-09-22 20:13:02 +0900

    수정

--- index.js
+++ index.js
@@ -16,19 +16,16 @@ express()
   .get("/", (req, res) => res.render("pages/index"))
   .post("/slack/events", (req, res) => {
     let body = req.body;
-    let event = req.event;
-
+    let event = body.event;
     if (body.type === "event_callback") {
       if (event.type === "message") {
+        // 메시지 이벤트인 경우, 메시지가 '안녕'이면 '안녕하세요' 메시지 전송
         if (event.text === "안녕") {
           console.log(
-            `인사 메세지 수신 channel:${event.channel}, user:${event.user}`
+            `인사 메시지 수신 channel:${event.channel}, user:${event.user}`
           );
           web.chat
-            .postMessage({
-              channel: event.channel,
-              text: "안녕하쇼",
-            })
+            .postMessage({ channel: event.channel, text: "안녕하세요." })
             .then((result) => {
               console.log("Message sent: " + result.ts);
             });
@@ -36,6 +33,7 @@ express()
         }
       }
     } else if (body.type === "url_verification") {
+      // URL 검증을 위한 처리
       console.log("url verification");
       res.send(body.challenge);
     } else {
